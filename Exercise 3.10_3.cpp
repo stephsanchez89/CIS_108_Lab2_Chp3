1ba32fb0f76b134de9d74ef5b9681eb145f39e52
stephsanchez89 <60125837+stephsanchez89@users.noreply.github.com>
2020-01-29 12:55:27 -0800

    Add files via upload
    Chapter 3 exercises

---
+++ Exercise 3.10_3.cpp
@@ -0,0 +1,26 @@
+// Exercise 3.10_3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+/*  Find the circle
+Measure from one side to the other side with a line going straight through
+The measurement from one side to the other is the diameter.
+Then measure from the diameter line to the edge of the circle.
+The line from the diameter to the edge of the circle is the radius.
+The circumfrence of the circle is the length of the line of the circle.
+pie = 3.1416
+To solve for the radius of the circle.r = c / 2pie */
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
